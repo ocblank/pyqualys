c704e399eacb4c5e61a7702ca6af819324ca15d6
Amit Ghadge <amitg.b14@gmail.com>
2018-05-18 18:57:47 +0530

    added asset example template

---
+++ example/example_asset.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+def add_asset():
+    pass
+
+
+def update_asset():
+    pass
+
+
+def delete_asset():
+    pass
+
+
+def list_assets():
+    pass
+
+
+def search_asset():
+    pass
