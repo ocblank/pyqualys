03ac3ee688ecd2f92e62ce77cfa8069ed623c97f
Amit Ghadge <amitg.b14@gmail.com>
2018-06-18 22:18:37 +0530

    add report

---
+++ pyqualys/services/report/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+
+
+def ReportFormat(func, format=None):
+    def inner(*args, **kwargs):
+        output = func(*args, **kwargs)
+        
+    return inner
