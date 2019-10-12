673d9c750e939e734b93811856f1e7e679904a5b
Amitgb14 <amitg.b14@gmail.com>
2019-10-12 23:56:20 +0530

    small changes
    Signed-off-by: Amitgb14 <amitg.b14@gmail.com>

--- example/example_asset_group.py
+++ example/example_asset_group.py
@@ -14,7 +14,7 @@ new_asset_group = service.add_asset(title="myLinux", ips="10.10.10.1")
 print("Add asset group: %s" % new_asset_group)
 
 # Update Asset Group
-update_asset_group = service.update_asset(asset_group_id=asset_group_id, title="myOS", ips="10.10.10.1")
+update_asset_group = service.update_asset(asset_group_id=1234, title="myOS", ips="10.10.10.1")
 print("Update asset group: %s" % update_asset_group)
 
 # List of Asset Group
@@ -22,9 +22,9 @@ all_assets_group = service.list_assets()
 print("List out all assets group: %s" % all_assets_group)
 
 # Get Asset Group
-get_asset_group = service.search_asset(ids=asset_id)
+get_asset_group = service.search_asset(ids=1234)
 print("Get asset group: %s" % get_asset_group)
 
 # Delete Asset Group
-delete_asset_group = service.delete_asset(asset_id=asset_id)
+delete_asset_group = service.delete_asset(asset_id=1234)
 print("Delete asset group: %s" % delete_asset_group)
